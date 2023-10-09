c5ef6002079eaa9c2fab2c0f3f08281a32abafc3
Ricardo <ricardogomezramos@gmail.com>
2023-10-09 18:37:27 +0200

    Añado ventanas y navego entre ellas

--- Varios_Activity_Con_Datos/app/src/main/java/com/example/variosactivitypasodatos/MainActivity.kt
+++ Varios_Activity_Con_Datos/app/src/main/java/com/example/variosactivitypasodatos/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.variosactivitypasodatos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.variosactivitypasodatos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.boton.setOnClickListener{
+            irVentana2()
+        }
+    }
+
+
+    private fun irVentana2(){
+        //podriamos crear aqui el objeto y pasarlo, en vez de los atributos individualmente
+        //persona deberia ser serializable en ese caso
+        var miIntent: Intent = Intent(this, Ventana2::class.java)
+        //miIntent.putExtra("nombre", binding.cajaNombre.txt.toString())
+        //miIntent.putExtra("edad", binding.cajaEdad.txt.toString())
+        startActivity(miIntent)
+
     }
 }
+
---
+++ Varios_Activity_Con_Datos/app/src/main/java/com/example/variosactivitypasodatos/Ventana2.kt
@@ -0,0 +1,11 @@
+package com.example.variosactivitypasodatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ventana2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ventana2)
+    }
+}
