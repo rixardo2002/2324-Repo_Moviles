a62b3ec904827162e0983dfaf0cbbf342080b989
Ricardo <ricardogomezramos@gmail.com>
2023-11-19 11:59:56 +0100

    Clases user, rol,proveedor

---
+++ MyGardenRGR/app/src/main/java/com/example/mygardenrgr/Home.kt
@@ -0,0 +1,45 @@
+package com.example.mygardenrgr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.mygardenrgr.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+
+
+class Home : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+    private lateinit var firebaseauth : FirebaseAuth
+    val TAG = "ACSCO"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Para la autenticación, de cualquier tipo.
+        firebaseauth = FirebaseAuth.getInstance()
+
+        //Recuperamos los datos del login.
+        binding.txtEmail.text = intent.getStringExtra("email").toString()
+        binding.txtProveedor.text = intent.getStringExtra("provider").toString()
+        binding.txtNombre.text = intent.getStringExtra("nombre").toString()
+
+        binding.btCerrarSesion.setOnClickListener {
+            Log.e(TAG, firebaseauth.currentUser.toString())
+            // Olvidar al usuario, limpiando cualquier referencia persistente
+            //comprobadlo en Firebase, como ha desaparecido.
+//            firebaseauth.currentUser?.delete()?.addOnCompleteListener { task ->
+//                if (task.isSuccessful) {
+//                    firebaseauth.signOut()
+//                    Log.e(TAG,"Cerrada sesión completamente")
+//                } else {
+//                    Log.e(TAG,"Hubo algún error al cerrar la sesión")
+//                }
+//            }
+            firebaseauth.signOut()
+            finish()
+        }
+    }
+}
---
+++ MyGardenRGR/app/src/main/java/com/example/mygardenrgr/Rol.kt
@@ -0,0 +1,6 @@
+package com.example.mygardenrgr
+
+enum class Rol {
+    ADMIN,
+    BASIC
+}
