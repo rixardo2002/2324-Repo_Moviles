d9859fc4d1d75e0584937b00c238e9f12625f3e6
Ricardo <ricardogomezramos@gmail.com>
2023-11-19 11:49:18 +0100

    First Commit

--- MyGardenRGR/app/src/main/java/com/example/mygardenrgr/MainActivity.kt
+++ MyGardenRGR/app/src/main/java/com/example/mygardenrgr/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.mygardenrgr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mygardenrgr.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseauth : FirebaseAuth
+    //private lateinit var googleSignInClient: GoogleSignInClient
+    val TAG = "ACSCO"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
     }
 }
